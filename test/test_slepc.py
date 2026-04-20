ddb956085e694f8c836f9e96e93f1b76e7c50307
sourav-condmat <sourav.offc@gmail.com>
2026-04-20 14:32:24 +0200

    update slepc test to skip windows

--- test/test_slepc.py
+++ test/test_slepc.py
@@ -1,7 +1,13 @@
+import sys
 import pytest
+
+pytestmark = pytest.mark.skipif(
+    sys.platform == "win32",
+    reason="SLEPc not supported on Windows"
+)
+
 from petsc4py import PETSc
 from slepc4py import SLEPc
-
 
 def test_slepc_eigenvalues():
     # Create a simple 2x2 matrix
@@ -9,7 +15,6 @@ def test_slepc_eigenvalues():
     A.setSizes([2, 2])
     A.setFromOptions()
     A.setUp()
-
     A[0, 0] = 2.0
     A[0, 1] = 1.0
     A[1, 0] = 1.0
@@ -20,7 +25,6 @@ def test_slepc_eigenvalues():
     E = SLEPc.EPS().create()
     E.setOperators(A)
     E.setProblemType(SLEPc.EPS.ProblemType.HEP)
-
     E.solve()
 
     nconv = E.getConverged()
@@ -28,14 +32,11 @@ def test_slepc_eigenvalues():
 
     vr, vi = A.getVecs()
     eigvals = []
-
     for i in range(nconv):
         eig = E.getEigenpair(i, vr, vi)
         eigvals.append(eig.real)
-
     eigvals.sort()
 
     # Known eigenvalues of [[2,1],[1,3]]
     assert pytest.approx(eigvals[0], rel=1e-6) == 1.381966
     assert pytest.approx(eigvals[1], rel=1e-6) == 3.618034
-
